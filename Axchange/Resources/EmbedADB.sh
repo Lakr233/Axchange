488adc62d40953ac0d63068a9bab913490cf6a42
砍砍 <git@qaq.wiki>
2024-10-29 16:18:34 +0800

    No Longer Modifies ADB

--- Axchange/Resources/EmbedADB.sh
+++ Axchange/Resources/EmbedADB.sh
@@ -7,9 +7,14 @@ echo "    PROJECT_DIR: $PROJECT_DIR"
 echo "    CONFIGURATION: $CONFIGURATION"
 echo "    CODE_SIGN_IDENTITY: $CODE_SIGN_IDENTITY"
 echo "    DEVELOPMENT_TEAM: $DEVELOPMENT_TEAM"
+echo "    CODESIGNING_FOLDER_PATH: $CODESIGNING_FOLDER_PATH"
 
 cd "$(dirname "$0")"
 
+APP_PATH="$CODESIGNING_FOLDER_PATH"
+cp -f ./adb "$APP_PATH/Contents/MacOS/adb"
+
+cd "$APP_PATH/Contents/MacOS"
 codesign --force --sign "$CODE_SIGN_IDENTITY" \
     --entitlements "./Entitlements-Subprocess.entitlements" \
     -o runtime \
