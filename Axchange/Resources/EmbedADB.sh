cc2f00cb2c913abe4f5b0c75506f71a496b537cb
砍砍 <git@qaq.wiki>
2024-10-30 15:22:31 +0800

    Update EmbedADB.sh

--- Axchange/Resources/EmbedADB.sh
+++ Axchange/Resources/EmbedADB.sh
@@ -3,10 +3,8 @@
 set -e
 set -o pipefail
 
-echo "    PROJECT_DIR: $PROJECT_DIR"
-echo "    CONFIGURATION: $CONFIGURATION"
 echo "    CODE_SIGN_IDENTITY: $CODE_SIGN_IDENTITY"
-echo "    DEVELOPMENT_TEAM: $DEVELOPMENT_TEAM"
+echo "    EXPANDED_CODE_SIGN_IDENTITY_NAME: $EXPANDED_CODE_SIGN_IDENTITY_NAME"
 echo "    CODESIGNING_FOLDER_PATH: $CODESIGNING_FOLDER_PATH"
 
 cd "$(dirname "$0")"
@@ -14,6 +12,11 @@ ENTITLEMENT_PATH="$(pwd)/Entitlements-Subprocess.entitlements"
 
 APP_PATH="$CODESIGNING_FOLDER_PATH"
 cp -f ./adb "$APP_PATH/Contents/MacOS/adb"
+
+if [ -n "$EXPANDED_CODE_SIGN_IDENTITY_NAME" ]; then
+    echo "[*] overwrite CODE_SIGN_IDENTITY to $EXPANDED_CODE_SIGN_IDENTITY_NAME"
+    CODE_SIGN_IDENTITY="$EXPANDED_CODE_SIGN_IDENTITY_NAME"
+fi
 
 cd "$APP_PATH/Contents/MacOS"
 codesign --force --sign $CODE_SIGN_IDENTITY \
